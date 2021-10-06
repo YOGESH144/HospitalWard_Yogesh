be094f292610229b07ef0f646e66d3ef1c1656ec
Sachinsaini737448@gmail.com <Sachinsaini737448@gmail.com>
2021-10-06 22:31:53 +0300

    Add project files.

---
+++ HospitalWard_Yogesh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HospitalWard_Yogesh.Startup))]
+namespace HospitalWard_Yogesh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
